dd10dc96d24ee81c8ea6cfe386dd11dcc13243c2
hhu <hhu@thoughtworks.com>
2019-03-24 16:17:40 +0800

    create spring-with-jpa project

---
+++ spring-with-jpa/src/main/java/study/huhao/name/springwithjpa/SpringWithJpaApplication.java
@@ -0,0 +1,13 @@
+package study.huhao.name.springwithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithJpaApplication.class, args);
+    }
+
+}
---
+++ spring-with-jpa/src/test/java/study/huhao/name/springwithjpa/SpringWithJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package study.huhao.name.springwithjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWithJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
