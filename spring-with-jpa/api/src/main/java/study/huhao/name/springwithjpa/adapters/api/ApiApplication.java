03d0954b43ec9c1a2c61edbf0550745fe69bb907
hhu <hhu@thoughtworks.com>
2019-03-24 20:19:46 +0800

    rename application

--- spring-with-jpa/api/src/main/java/study/huhao/name/springwithjpa/adapters/api/ApiApplication.java
+++ spring-with-jpa/api/src/main/java/study/huhao/name/springwithjpa/adapters/api/ApiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "study.huhao.name.springwithjpa")
-public class SpringWithJpaApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWithJpaApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
--- spring-with-jpa/api/src/test/java/study/huhao/name/springwithjpa/adapters/api/ApiApplicationTests.java
+++ spring-with-jpa/api/src/test/java/study/huhao/name/springwithjpa/adapters/api/ApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class SpringWithJpaApplicationTests {
+class ApiApplicationTests {
 
     @Test
     void contextLoads() {
