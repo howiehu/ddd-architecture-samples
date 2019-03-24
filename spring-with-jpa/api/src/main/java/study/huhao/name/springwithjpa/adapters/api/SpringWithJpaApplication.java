c057e2ae8ab4d0de46029db327ce35036c26ae19
hhu <hhu@thoughtworks.com>
2019-03-24 20:18:40 +0800

    fix bugs

--- spring-with-jpa/api/src/main/java/study/huhao/name/springwithjpa/adapters/api/SpringWithJpaApplication.java
+++ spring-with-jpa/api/src/main/java/study/huhao/name/springwithjpa/adapters/api/SpringWithJpaApplication.java
@@ -2,12 +2,14 @@ package study.huhao.name.springwithjpa.adapters.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class ApiAdapter {
+@ComponentScan(basePackages = "study.huhao.name.springwithjpa")
+public class SpringWithJpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiAdapter.class, args);
+        SpringApplication.run(SpringWithJpaApplication.class, args);
     }
 
 }
---
+++ spring-with-jpa/api/src/test/java/study/huhao/name/springwithjpa/adapters/api/SpringWithJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package study.huhao.name.springwithjpa.adapters.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SpringWithJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
