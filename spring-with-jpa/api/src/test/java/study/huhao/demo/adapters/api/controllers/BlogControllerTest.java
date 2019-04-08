69e823ccaebddb8c280df2008cc1e7a95143c16a
hhu <hhu@thoughtworks.com>
2019-04-08 22:18:07 +0800

    change api test to e2e test

--- spring-with-jpa/api/src/test/java/study/huhao/demo/adapters/api/controllers/BlogControllerTest.java
+++ spring-with-jpa/api/src/test/java/study/huhao/demo/adapters/api/controllers/BlogControllerTest.java
@@ -1,6 +1,5 @@
 package study.huhao.demo.adapters.api.controllers;
 
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.flywaydb.test.FlywayTestExecutionListener;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,37 +7,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.transaction.annotation.Transactional;
-import study.huhao.demo.application.services.BlogService;
 
 import java.util.Map;
 import java.util.UUID;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.RestAssured.given;
+
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class})
-@FlywayTest
-@Transactional
 @DisplayName("/blogs")
 class BlogControllerTest {
 
-    @Autowired
-    private BlogService blogService;
+    @LocalServerPort
+    private int port;
 
     @BeforeEach
+    @FlywayTest
     void setUp() {
-        RestAssuredMockMvc.standaloneSetup(new BlogController(blogService));
     }
 
     @Nested
@@ -47,7 +41,8 @@ class BlogControllerTest {
 
         @Test
         void should_create_blog() {
-            given().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+            given().port(port)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                     .body(Map.of(
                             "title", "Test Blog",
                             "body", "Something...",
