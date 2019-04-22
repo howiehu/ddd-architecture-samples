2b399504dd481c11bcb906fd016648d704894630
hhu <hhu@thoughtworks.com>
2019-04-22 23:50:58 +0800

    Revert "remove jersey-with-mybatis demo"
    This reverts commit 40059a3249f72e5f986100389df4779c4ffebd6e.

---
+++ jersey-with-mybatis/src/main/java/study/huhao/demo/JerseyWithMybatisApplication.java
@@ -0,0 +1,13 @@
+package study.huhao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseyWithMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JerseyWithMybatisApplication.class, args);
+    }
+
+}
