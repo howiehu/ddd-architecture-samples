31238f6049e0df94b61af32461f8b7a5ead9e0c8
hhu <hhu@thoughtworks.com>
2019-04-24 12:54:15 +0800

    [spring-with-jpa] fix spelling issue

--- spring-with-jpa/src/main/java/study/huhao/demo/domain/core/WriteModel.java
+++ spring-with-jpa/src/main/java/study/huhao/demo/domain/core/WriteModel.java
@@ -1,7 +1,7 @@
 package study.huhao.demo.domain.core;
 
 // TODO: We usually will directly use the domain model as the write model.
-//  But in somethings, read then save a large aggregate may have some performance issue.
+//  But in sometimes, read then save a large aggregate may have some performance issue.
 //  So, maybe we can use the read model and the write model to resolve this problem. The idea needs to be verified.
 public interface WriteModel {
 }
