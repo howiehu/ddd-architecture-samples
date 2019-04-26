d18ada7a6f2803d922bad8676e2a7033d741c901
hhu <hhu@thoughtworks.com>
2019-04-26 19:26:44 +0800

    renaming database

---
+++ java/jersey-with-mybatis/src/main/resources/db/migration/V20190406234633__create_blog_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `blog`
+(
+    `id`              CHAR(36) PRIMARY KEY,
+    `title`           VARCHAR(80),
+    `body`            TEXT,
+    `author_id`       CHAR(36),
+    `status`          VARCHAR(16),
+    `created_at`      TIMESTAMP(6),
+    `saved_at`        TIMESTAMP(6),
+    `published_title` VARCHAR(80),
+    `published_body`  TEXT,
+    `published_at`    TIMESTAMP(6)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE utf8mb4_bin;
