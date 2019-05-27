745feee0bedba3afb0793a4e8a2216a470986197
hhu <hhu@thoughtworks.com>
2019-05-27 14:20:18 +0800

    [java] enable transaction on application

--- java/spring-with-jpa/src/main/java/study/huhao/demo/SpringWithJpaApplication.java
+++ java/spring-with-jpa/src/main/java/study/huhao/demo/SpringWithJpaApplication.java
@@ -3,8 +3,10 @@ package study.huhao.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @PropertySource("classpath:flyway.properties")
 public class SpringWithJpaApplication {
 
