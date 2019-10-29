7865d94c453dcdb4a264b379c6e89bf4d8d05b4e
hhu <hhu@thoughtworks.com>
2019-10-29 09:35:16 +0800

    add comments to UseCase & Factory

--- spring-with-mybatis/src/main/java/study/huhao/demo/application/concepts/UseCase.java
+++ spring-with-mybatis/src/main/java/study/huhao/demo/application/concepts/UseCase.java
@@ -1,4 +1,15 @@
 package study.huhao.demo.application.concepts;
 
+/**
+ * UseCase（用例）用于承载应用场景和应用需求，以及其他Domain层无法承载的逻辑，例如：
+ *
+ * <ul>
+ *     <li>应用需求的输入验证（业务需要的输入验证应放在Domain层中）</li>
+ *     <li>依赖注入框架</li>
+ *     <li>事务</li>
+ *     <li>跨上下文调用</li>
+ *     <li>其他应用层面的依赖</li>
+ * </ul>
+ */
 public interface UseCase {
 }
--- spring-with-mybatis/src/main/java/study/huhao/demo/domain/core/concepts/Factory.java
+++ spring-with-mybatis/src/main/java/study/huhao/demo/domain/core/concepts/Factory.java
@@ -1,4 +1,7 @@
 package study.huhao.demo.domain.core.concepts;
 
+/**
+ * 工厂用于业务上需要多种复杂创建逻辑的实体，如果不存在多种复杂创建业务逻辑，应优先考虑使用实体的构造函数来代替工厂。
+ */
 public interface Factory {
 }
