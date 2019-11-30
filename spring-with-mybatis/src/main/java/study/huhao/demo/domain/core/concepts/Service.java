4ab73c533c666239381789bf4beb3dbb0a1d5ad4
Hu Hao <huhao@outlook.com>
2019-11-30 08:47:44 +0800

    add java doc comment to domain service interface

--- spring-with-mybatis/src/main/java/study/huhao/demo/domain/core/concepts/Service.java
+++ spring-with-mybatis/src/main/java/study/huhao/demo/domain/core/concepts/Service.java
@@ -1,4 +1,7 @@
 package study.huhao.demo.domain.core.concepts;
 
+/**
+ * 领域服务（Domain Service）可以视作 Domain 层聚合的统一对外接口，用于封装聚合的行为（这样聚合根就可以 Immutable），实现持久化和对于 Domain Model 的守护。
+ */
 public interface Service {
 }
