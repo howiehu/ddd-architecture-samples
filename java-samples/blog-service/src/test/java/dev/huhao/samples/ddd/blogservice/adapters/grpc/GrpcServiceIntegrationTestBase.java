4b531164f85142597eff612c6f29ac11659d041d
Hao Hu <huhao@outlook.com>
2020-05-02 17:28:23 +0800

    use yml properties file

--- java-samples/blog-service/src/test/java/dev/huhao/samples/ddd/blogservice/adapters/grpc/GrpcServiceIntegrationTestBase.java
+++ java-samples/blog-service/src/test/java/dev/huhao/samples/ddd/blogservice/adapters/grpc/GrpcServiceIntegrationTestBase.java
@@ -4,11 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@SpringBootTest(properties = {
-        "grpc.server.inProcessName=test", // Enable inProcess server
-        "grpc.server.port=-1", // Disable external server
-        "grpc.client.inProcess.address=in-process:test" // Configure the client to connect to the inProcess server
-})
+@SpringBootTest
 @SpringJUnitConfig(classes = {GrpcServiceIntegrationTestConfiguration.class})
 // Ensures that the grpc-server is properly shutdown after each test
 // Avoids "port already in use" during tests
