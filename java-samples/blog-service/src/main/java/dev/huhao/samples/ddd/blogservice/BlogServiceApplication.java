58cf95cceb33f8a205b6212fdaa21ddce397d17d
Hao Hu <huhao@outlook.com>
2020-05-02 13:51:50 +0800

    init blog-service project

---
+++ java-samples/blog-service/src/main/java/dev/huhao/samples/ddd/blogservice/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.huhao.samples.ddd.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServiceApplication.class, args);
+    }
+
+}
---
+++ java-samples/blog-service/src/test/java/dev/huhao/samples/ddd/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.huhao.samples.ddd.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
