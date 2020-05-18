8be52becc30156300d28b565cb139d0adb463596
Hao Hu <huhao@outlook.com>
2020-05-18 16:46:19 +0800

    bff project init

---
+++ java-samples/backend-for-frontend/src/main/java/dev/huhao/samples/ddd/bff/BackendForFrontendApplication.java
@@ -0,0 +1,13 @@
+package dev.huhao.samples.ddd.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendForFrontendApplication.class, args);
+    }
+
+}
---
+++ java-samples/backend-for-frontend/src/test/java/dev/huhao/samples/ddd/bff/BackendForFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.huhao.samples.ddd.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
